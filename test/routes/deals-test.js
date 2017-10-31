c104556aee144bca8d184801e293e50160284e8b
Tarek Huawei <tarek.huawei@yahoo.com>
2017-10-31 15:07:35 +0100

    Get deals endpoint tested

--- test/routes/deals-test.js
+++ test/routes/deals-test.js
@@ -5,3 +5,26 @@ var expect = chai.expect;
 
 chai.use(chaiHttp);
 var _ = require('lodash' );
+chai.use(require('chai-things'));
+
+describe('Deals', function (){
+      describe('GET /deals', function () {
+              it('should return all the deals in an array', function(done) {
+                    chai.request(server)
+                        .get('/deals')
+                        .end(function(err, res) {
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('array');
+      expect(res.body.length).to.equal(2);
+      var result = _.map(res.body, function (deal) {
+          return { id: deal.id,
+              name: deal.name } 
+          });
+      expect(result).to.include( { id: 1000000, name: 'Early bird'  } );
+      expect(result).to.include( { id: 1000001, name: 'Premium movie for 2'  } );
+      done();
+  });
+              });
+        });
+
+  });
